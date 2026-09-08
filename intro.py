47f9a5808821c092348ca7200e5312891de01f36
Nikhil <rajn29821@gmail.com>
2026-09-08 21:04:32 +0530

    assignment

---
+++ intro.py
@@ -0,0 +1,6 @@
+print("Hello")
+print("My Name is Nikhil")
+print("I live in Bilaspur")
+print("My Favourite subject is Python")
+peint("I like python because it is problem solving language")
+10+20//2*34+23
